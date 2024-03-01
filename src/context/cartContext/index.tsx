1f777260b2ea07d81f1baebbbf5c3972e17ca0df
Chakphet Wongmanee <king_23619@hotmail.com>
2024-03-01 16:45:15 +0700

    เพิ่มไฟล์ context.tsx

---
+++ src/context/cartContext/index.tsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import axios from '../../api'
+import {ProductType} from '../../data/product'
+
+interface Cart {
+    cartItems: ProductType[],
+    setCartItems: (data: ProductType[]) => void
+}
+
+const defaultCartContext: Cart = {
+    cartItems: [
+        {
+            id: 0,
+            image: '',
+            productName: '',
+            category: '',
+            price: 0,
+            scription: '',
+        }
+    ],
+    setCartItems: (_data) => { }
+}
+
+const CartConstex = createContext<Cart>(defaultCartContext)
+
+interface Props {
+    children: React.ReactNode
+}
+
+export function useCartContext() {
+    return useContext(CartConstex)
+}
+
+export default function CartConstexProvider({ children }: Props) {
+
+    const [cartItems, setCartItems] = useState<ProductType[]>([])
+
+    const getProductCart = async () => {
+        try {
+            await axios.get('/carts')
+            .then((result)=>{
+                setCartItems(result.data)
+            })
+        }
+        catch (error) {
+            throw error
+        }
+    }
+    useEffect(() => {
+        getProductCart()
+    },[])
+    
+    return (
+        <CartConstex.Provider value={{ cartItems, setCartItems }}>
+            {children}
+        </CartConstex.Provider>
+    )
+}
